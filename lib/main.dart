c89af85ef6ae42d69ac9cb9c50476b09812765a8
ohanz <Chiaguguadaniel@gmail.com>
2023-09-13 00:34:30 +0100

    added alarm widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_alarm_clock/flutter_alarm_clock.dart';
+
 
 void main() {
   runApp(const MyApp());
@@ -16,25 +18,10 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Popsi Alarm',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Popsi Alarm'),
     );
   }
 }
@@ -59,60 +46,152 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  bool show = false;
+  TextEditingController hourController = TextEditingController();
+  TextEditingController minuteController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
+
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body:
+      Center(
+          child: Column(children: <Widget>[
+            SizedBox(height: 30),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 40,
+                  width: 60,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.circular(11)),
+                  child: Center(
+                    child: TextField(
+                      controller: hourController,
+                      keyboardType: TextInputType.number,
+                    ),
+                  ),
+                ),
+                SizedBox(width: 20),
+                Container(
+                  height: 40,
+                  width: 60,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.rectangle,
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.circular(11)),
+                  child: Center(
+                    child: TextField(
+                      controller: minuteController,
+                      keyboardType: TextInputType.number,
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Text(
-              'Welcome!',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Container(
+              margin: const EdgeInsets.all(25),
+              child: TextButton(
+                child: const Text(
+                  'Create alarm',
+                  style: TextStyle(fontSize: 20.0),
+                ),
+                onPressed: () {
+                  int hour;
+                  int minutes;
+                  hour = int.parse(hourController.text);
+                  minutes = int.parse(minuteController.text);
+
+                  // creating alarm after converting hour
+                  // and minute into integer
+                  FlutterAlarmClock.createAlarm(hour, minutes);
+                },
+              ),
             ),
-          ],
-        ),
-      ),
+            ElevatedButton(
+              onPressed: () {
+
+                // show alarm
+                FlutterAlarmClock.showAlarms();
+              },
+              child: const Text(
+                'Show Alarms',
+                style: TextStyle(fontSize: 20.0),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(25),
+              child: TextButton(
+                  child: const Text(
+                    'Create timer',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                  onPressed: () {
+                    int minutes;
+                    minutes = int.parse(minuteController.text);
+
+                    // create timer
+                    FlutterAlarmClock.createTimer(minutes);
+                    showDialog(
+                        context: context,
+                        builder: (context) {
+                          return AboutDialog(
+                            children: [
+                              Center(
+                                child: Text("Timer is set",
+                                    style: TextStyle(
+                                        fontSize: 20, fontWeight: FontWeight.bold)),
+                              )
+                            ],
+                          );
+                        });
+                  }),
+            ),
+            ElevatedButton(
+              onPressed: () {
+
+                // show timers
+                FlutterAlarmClock.showTimers();
+              },
+              child: Text(
+                "Show Timers",
+                style: TextStyle(fontSize: 17),
+              ),
+            )
+          ])),
+      // Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     children: <Widget>[
+      //       const Text(
+      //         'hye',
+      //       ),
+      //       Text(
+      //         'Welcome!',
+      //         style: Theme.of(context).textTheme.headlineMedium,
+      //       ),
+      //     ],
+      //   ),
+      // ),
       floatingActionButton: FloatingActionButton(
         onPressed: (){
-
+          setState(() {
+            if(show) {
+              show = false;
+            } else {
+              show = true;
+            }
+          });
         },
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        tooltip: 'Show/hide',
+        child: show?const Icon(Icons.man_4_rounded):const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
